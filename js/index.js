80d728bde630f8c054ba1fb48069cab84e4e9673
Anthony Rurup <tcrurup@gmail.com>
2019-09-27 22:35:53 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -12,37 +12,6 @@ class User {
     }
  
     matchInterests(event) {
-        console.log("'this' is defined: ", this);
-        return event.keywords.some(function(word) {
-            console.log("'this' is now undefined: ", this);
-            return this.interests.includes(word);
-        });
-    }
-}
- 
-let billy = new User('billy', ['music', 'art', 'movies']);
-let freeMusic = new Event('Free Music Show', ['music', 'free', 'outside']);
- 
-billy.matchInterests(freeMusic);
-// 'this' is defined:  User {name: "billy", interests: Array(3)}
-// 'this' is now undefined:  undefined
-// Uncaught TypeError: Cannot read property 'interests' of undefined
-In the first console.log, this refers to the billy user instance. In the second, this is undefined. To solve this problem, we can use bind:
-
-class Event {
-    constructor(title, keywords) {
-        this.title = title;
-        this.keywords = keywords;
-    }
-}
- 
-class User {
-    constructor(name, interests) {
-        this.name = name;
-        this.interests = interests;
-    }
- 
-    matchInterests(event) {
         return event.keywords.some(
             function(word) {
                 return this.interests.includes(word);
