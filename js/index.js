ecf9d777d17515eb50da12d5cdc53f48fd5f8199
Anthony Rurup <tcrurup@gmail.com>
2019-09-27 22:35:59 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
-// use this to copy code snippets or use your browser's console
+class Event {
+    constructor(title, keywords) {
+        this.title = title;
+        this.keywords = keywords;
+    }
+}
+ 
+class User {
+    constructor(name, interests) {
+        this.name = name;
+        this.interests = interests;
+    }
+ 
+    matchInterests(event) {
+        return event.keywords.some(
+            function(word) {
+                return this.interests.includes(word);
+            }.bind(this) // added to the and of the callback function
+        );
+    }
+}
